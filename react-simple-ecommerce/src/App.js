97b2bed7845047985b34dcba8fad4b6368daa566
Letitia Treasure <letitia.treasure@vodafone.com>
2021-08-16 17:27:44 +0100

    start up code

---
+++ react-simple-ecommerce/src/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Luna</h1>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
